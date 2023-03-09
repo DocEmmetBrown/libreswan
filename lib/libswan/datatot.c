1ce8c177cd261298df203bbc5c70b99ca6bc068c
Paul Wouters <paul.wouters@aiven.io>
2023-03-09 12:10:37 -0500

    libswan: change a strcpy to strncpy
    Mostly to make codeql happy.

--- lib/libswan/datatot.c
+++ lib/libswan/datatot.c
@@ -26,21 +26,21 @@ static void convert(const char *src, size_t nreal, int format, char *out);
  * true length (with NUL) for success
  */
 size_t datatot(const void *src, size_t srclen,
-	       int format,  	  /* character indicating what format */
-	       char *dst, 	  /* need not be valid if dstlen is 0 */
-	       size_t dstlen)
+							 int format, /* character indicating what format */
+							 char *dst,	 /* need not be valid if dstlen is 0 */
+							 size_t dstlen)
 {
-	size_t inblocksize;             /* process this many bytes at a time */
-	size_t outblocksize;            /* producing this many */
-	size_t breakevery;              /* add a _ every this many (0 means don't) */
-	size_t sincebreak;              /* output bytes since last _ */
-	char breakchar;                 /* character used to break between groups */
-	unsigned char inblock[10];      /* enough for any format */
-	char outblock[10];              /* enough for any format */
-	char fake[1];                   /* fake output area for dstlen == 0 */
-	size_t needed;                  /* return value */
-	char *stop;                     /* where the terminating NUL will go */
-	size_t ntodo;                   /* remaining input */
+	size_t inblocksize;				 /* process this many bytes at a time */
+	size_t outblocksize;			 /* producing this many */
+	size_t breakevery;				 /* add a _ every this many (0 means don't) */
+	size_t sincebreak;				 /* output bytes since last _ */
+	char breakchar;						 /* character used to break between groups */
+	unsigned char inblock[10]; /* enough for any format */
+	char outblock[10];				 /* enough for any format */
+	char fake[1];							 /* fake output area for dstlen == 0 */
+	size_t needed;						 /* return value */
+	char *stop;								 /* where the terminating NUL will go */
+	size_t ntodo;							 /* remaining input */
 	size_t nreal;
 	char *out;
 	char *prefix;
@@ -48,7 +48,8 @@ size_t datatot(const void *src, size_t srclen,
 	breakevery = 0;
 	breakchar = '_';
 
-	switch (format) {
+	switch (format)
+	{
 	case 0:
 	case 'h':
 		breakevery = 8;
@@ -81,7 +82,7 @@ size_t datatot(const void *src, size_t srclen,
 		outblocksize = 4;
 		prefix = "0s";
 		break;
-	case 64:                /* beware, equals ' ' */
+	case 64: /* beware, equals ' ' */
 		inblocksize = 3;
 		outblocksize = 4;
 		prefix = "";
@@ -100,35 +101,43 @@ size_t datatot(const void *src, size_t srclen,
 
 	ntodo = srclen;
 
-	if (dstlen == 0) {      /* dispose of awkward special case */
+	if (dstlen == 0)
+	{ /* dispose of awkward special case */
 		dst = fake;
 		dstlen = 1;
 	}
 	stop = dst + dstlen - 1;
 
 	nreal = strlen(prefix);
-	needed = nreal;                 /* for starters */
-	if (dstlen <= nreal) {          /* prefix won't fit */
+	needed = nreal; /* for starters */
+	if (dstlen <= nreal)
+	{ /* prefix won't fit */
 		/* fill_and_terminate */
-		strncpy(dst, prefix, dstlen-1);
-		dst[dstlen-1] = '\0';
+		strncpy(dst, prefix, dstlen - 1);
+		dst[dstlen - 1] = '\0';
 		dst += dstlen - 1;
-	} else {
-		strcpy(dst, prefix); /*dstlen > strlen(prefix) */
+	}
+	else
+	{
+		strncpy(dst, prefix, dstlen); /* make codeql happy */
 		dst += nreal;
 	}
 
 	passert(dst <= stop);
 	sincebreak = 0;
 
-	while (ntodo > 0) {
-		if (ntodo < inblocksize) {      /* incomplete input */
+	while (ntodo > 0)
+	{
+		if (ntodo < inblocksize)
+		{ /* incomplete input */
 			memset(inblock, 0, sizeof(inblock));
 			memcpy(inblock, src, ntodo);
 			src = inblock;
 			nreal = ntodo;
 			ntodo = inblocksize;
-		} else {
+		}
+		else
+		{
 			nreal = inblocksize;
 		}
 		out = ((ptrdiff_t)outblocksize > stop - dst) ? outblock : dst;
@@ -136,20 +145,25 @@ size_t datatot(const void *src, size_t srclen,
 		convert((const char *)src, nreal, format, out);
 		needed += outblocksize;
 		sincebreak += outblocksize;
-		if (dst < stop) {
-			if (out != dst) {
+		if (dst < stop)
+		{
+			if (out != dst)
+			{
 				passert(
-					(ptrdiff_t)outblocksize > stop - dst);
+						(ptrdiff_t)outblocksize > stop - dst);
 				memcpy(dst, out, stop - dst);
 				dst = stop;
-			} else {
+			}
+			else
+			{
 				dst += outblocksize;
 			}
 		}
 
 		src += inblocksize;
 		ntodo -= inblocksize;
-		if (breakevery != 0 && sincebreak >= breakevery && ntodo > 0) {
+		if (breakevery != 0 && sincebreak >= breakevery && ntodo > 0)
+		{
 			if (dst < stop)
 				*dst++ = breakchar;
 			needed++;
@@ -165,21 +179,22 @@ size_t datatot(const void *src, size_t srclen,
 }
 
 /*
-   - convert - convert one input block to one output block
+	 - convert - convert one input block to one output block
  */
 static void convert(const char *src,
-		    size_t nreal,    /* how much of the input block is real */
-		    int format, char *out)
+										size_t nreal, /* how much of the input block is real */
+										int format, char *out)
 {
 	static char hex[] = "0123456789abcdef";
-	static char base64[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-			       "abcdefghijklmnopqrstuvwxyz"
-			       "0123456789+/";
+	static char base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+												 "abcdefghijklmnopqrstuvwxyz"
+												 "0123456789+/";
 	unsigned char c;
 	unsigned char c1, c2, c3;
 
 	passert(nreal > 0);
-	switch (format) {
+	switch (format)
+	{
 	case 'x':
 		passert(nreal == 1);
 		c = (unsigned char)*src;
@@ -190,24 +205,27 @@ static void convert(const char *src,
 		c1 = (unsigned char)*src++;
 		c2 = (unsigned char)*src++;
 		c3 = (unsigned char)*src++;
-		*out++ = base64[c1 >> 2];       /* top 6 bits of c1 */
-		c = (c1 & 0x3) << 4;            /* bottom 2 of c1... */
-		c |= c2 >> 4;                   /* ...top 4 of c2 */
+		*out++ = base64[c1 >> 2]; /* top 6 bits of c1 */
+		c = (c1 & 0x3) << 4;			/* bottom 2 of c1... */
+		c |= c2 >> 4;							/* ...top 4 of c2 */
 		*out++ = base64[c];
-		if (nreal == 1) {
+		if (nreal == 1)
+		{
 			*out++ = '=';
-		} else {
-			c = (c2 & 0xf) << 2;    /* bottom 4 of c2... */
-			c |= c3 >> 6;           /* ...top 2 of c3 */
+		}
+		else
+		{
+			c = (c2 & 0xf) << 2; /* bottom 4 of c2... */
+			c |= c3 >> 6;				 /* ...top 2 of c3 */
 			*out++ = base64[c];
 		}
 		if (nreal <= 2)
 			*out++ = '=';
 		else
-			*out++ = base64[c3 & 0x3f];     /* bottom 6 of c3 */
+			*out++ = base64[c3 & 0x3f]; /* bottom 6 of c3 */
 		break;
 	default:
-		passert(nreal == 0);        /* unknown format */
+		passert(nreal == 0); /* unknown format */
 		break;
 	}
 }
